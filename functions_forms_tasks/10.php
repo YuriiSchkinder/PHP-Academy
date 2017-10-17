68abb385ea70065894b8c0388b87087982979c1d
Yrii Schkinder <yurkaaa96@gmail.com>
2017-10-17 18:28:08 +0300

    Fixed 10 task

--- functions_forms_tasks/10.php
+++ functions_forms_tasks/10.php
@@ -5,25 +5,30 @@
 </form>
 <?php
 function world($a){
-$arr_w=explode(' ',$a);
-$arr=[];
-foreach ($arr_w as $key=>$val1){
-    foreach ($arr_w as $key1=>$val2) {
-        if($val1==$val2){
-            if(!isset($arr[$val2])){
-                $arr[$val2]=1;
-            }else{
-                $arr[$val2]++;
+    $arr_w=explode(' ',$a);
+    $arr=[];
+    foreach ($arr_w as $key=>$val1){
+        foreach ($arr_w as $key1=>$val2) {
+            if($val1==$val2){
+                if(!isset($arr[$val2])){
+                    $arr[$val2]=1;
+                }else{
+                    $arr[$val2]++;
+                }
+                unset($arr_w[$key1]);
             }
-            unset($arr_w[$key1]);
         }
-    }
 
     }
-return $arr;
+    $sum=0;
+    foreach ($arr as $value){
+        if($value==1){
+            $sum++;
+        }
+    }
+    unset($arr);
+    return $sum;
 }
 $foo=world($_POST['world']);
-foreach ($foo as $key=> $value){
-    echo "$key=>$value </br>";
-}
+echo "Унікальних слів=".$foo;
 ?>
