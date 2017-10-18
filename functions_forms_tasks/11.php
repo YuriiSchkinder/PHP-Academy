165f14a7a332bdf7fec59f53cfb1d9a3c5985c3c
Yrii Schkinder <yurkaaa96@gmail.com>
2017-10-18 23:52:04 +0300

    Fixed 11 Task

--- functions_forms_tasks/11.php
+++ functions_forms_tasks/11.php
@@ -1,10 +1,10 @@
 <?php
-$str="/mother has come.they said, summer would be very hot. anyone can do that";
+$str="a Васька слушает да ест. a воз и ныне там. А вы друзья как ни садитесь, все в музыканты не годитесь. a король-то — голый. a ларчик просто открывался.a там хоть трава не расти.";
 function upcase($string){
     $j=1;
     for($i=0;$i<=strlen($string)-1;$i++) {
         if ($j == 1&& $string[$i]>='a'&& $string[$i]<='z') {
-            $string[$i] = ucfirst($string[$i]);
+            $string[$i] = strtoupper($string[$i]);
             $j = 0;
         }elseif($string[$i]=='.'){
             $j=1;
