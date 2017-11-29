b7eea4ea09119502bd91e0e8e47e6680ddd3cdc7
Yurii_Shkinder <yurkaaa96@gmail.con>
2017-11-29 23:27:16 +0200

    10 task

--- mvctest/webroot/test.php
+++ mvctest/webroot/test.php
@@ -1,18 +1,12 @@
 <?php
 error_reporting(E_ALL);
-class Magic{
-    protected $v=[
-      'a'=>1,
-      'b'=>2,
-      'c'=>3
-    ];
-    public function &__get($v)
-    {
-        return $this->v[$v];
-    }
+
+class Bar{
+    private $a=false;
+    public $c=true;
 
 }
-$m= new Magic();
-$m->d[]=4;
-echo $m->d[0];
+$x=(array) new Bar();
+echo array_key_exists('a', $x)? 'true':'false'; echo '-';
+echo array_key_exists('c', $x)? 'true':'false';
 ?>
