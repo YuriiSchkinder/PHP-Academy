427584d0cc7291ea8f54797d00486633c7cecffd
Yurii_Shkinder <yurkaaa96@gmail.con>
2017-11-30 01:45:41 +0200

    28task

--- mvctest/webroot/test.php
+++ mvctest/webroot/test.php
@@ -1,24 +1,21 @@
 <?php
 error_reporting(E_ALL);
 
-trait Useful{
-    private $prop=Null;
-    public function increment(){
-        $this->prop++;
-    }
-    public function getValue(){
-        return $this->prop;
-    }
-}
-class MyClass{
-    use Useful;
-    public function incrementNew()
+abstract class Base {
+    protected function __construct()
     {
-        $this->increment();
-        $this->prop++;
+    }
+    public static function create(){
+        return new static();
+    }
+    abstract function action();
+}
+class Item extends Base {
+    public function action()
+    {
+     echo __CLASS__;   // TODO: Implement action() method.
     }
 }
-$c= new MyClass();
-$c->incrementNew();
-var_dump($c->getValue());
+$item= Item::create();
+$item->action();
 ?>
