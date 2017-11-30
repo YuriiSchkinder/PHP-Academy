d04c624ef8b6cb00665fbb9967e5909022c06ae4
Yurii_Shkinder <yurkaaa96@gmail.con>
2017-11-30 02:13:45 +0200

    34 task

--- mvctest/webroot/test.php
+++ mvctest/webroot/test.php
@@ -1,15 +1,12 @@
 <?php
 error_reporting(E_ALL);
 
-class Foo{
-    public static $my_st='foo';
+class Bar{
+    private $a='b';
+    public $c='d';
 }
-class Bar extends Foo{
-    public function fooStatic(){
-        return parent::$my_st;
-    }
-}
-$foo= new Bar();
-$foo->my_st= 'Bar';
-echo $foo->fooStatic();
+$x=(array) new Bar();
+echo array_key_exists('a',$x) ? 'true': 'false';
+echo '-';
+echo array_key_exists('c',$x) ? 'true': 'false';
 ?>
