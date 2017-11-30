aaff22b2341d476187cf917358d0a6f03803f826
Yurii_Shkinder <yurkaaa96@gmail.con>
2017-11-30 02:06:18 +0200

    32 task

--- mvctest/webroot/test.php
+++ mvctest/webroot/test.php
@@ -1,10 +1,15 @@
 <?php
 error_reporting(E_ALL);
 
-class MyClass{
-    static function muName(){
-        return __METHOD__;
+class Foo{
+    public static $my_st='foo';
+}
+class Bar extends Foo{
+    public function fooStatic(){
+        return parent::$my_st;
     }
 }
-print MyClass::muName();
+$foo= new Bar();
+$foo->my_st= 'Bar';
+echo $foo->fooStatic();
 ?>
